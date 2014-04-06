32a6a69c6f0f5b3b03ec14b3ec795e724b3e023c
Mohamed Elbou <medelbou@gmail.com>
2014-04-06 18:37:18 -0500

    formatting, added gitignore, updated images in places.js

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,8 +1,8 @@
 /*
-Constructor for the tooltip
-@ param options an object containing: marker(required), content(required) and cssClass(a css class, optional)
-@ see google.maps.OverlayView()
-*/
+ Constructor for the tooltip
+ @ param options an object containing: marker(required), content(required) and cssClass(a css class, optional)
+ @ see google.maps.OverlayView()
+ */
 
 function Tooltip(options) {
 
@@ -10,7 +10,7 @@ function Tooltip(options) {
     this.marker_ = options.marker;
     this.content_ = options.content;
     this.map_ = options.marker.get('map');
-	this.cssClass_ = options.cssClass||null;
+    this.cssClass_ = options.cssClass || null;
 
     // We define a property to hold the content's
     // div. We'll actually create this div
@@ -20,30 +20,30 @@ function Tooltip(options) {
 
     //Explicitly call setMap on this overlay
     this.setMap(this.map_);
-	var me = this;
-	// Show tooltip on mouseover event.
-	google.maps.event.addListener(me.marker_, 'mouseover', function() {
-		me.show();
-	});
-	// Hide tooltip on mouseout event.
-	google.maps.event.addListener(me.marker_, 'mouseout', function() {
-		me.hide();
-	});
+    var me = this;
+    // Show tooltip on mouseover event.
+    google.maps.event.addListener(me.marker_, 'mouseover', function () {
+        me.show();
+    });
+    // Hide tooltip on mouseout event.
+    google.maps.event.addListener(me.marker_, 'mouseout', function () {
+        me.hide();
+    });
 }
 // Now we extend google.maps.OverlayView()
 Tooltip.prototype = new google.maps.OverlayView();
 
 // onAdd is one of the functions that we must implement, 
 // it will be called when the map is ready for the overlay to be attached.
-Tooltip.prototype.onAdd = function() {
+Tooltip.prototype.onAdd = function () {
 
     // Create the DIV and set some basic attributes.
     var div = document.createElement('DIV');
     div.style.position = "absolute";
-	// Hide tooltip
-	div.style.visibility = "hidden";
-	if(this.cssClass_)
-		div.className += " "+this.cssClass_;
+    // Hide tooltip
+    div.style.visibility = "hidden";
+    if (this.cssClass_)
+        div.className += " " + this.cssClass_;
 
     //Attach content to the DIV.
     div.innerHTML = this.content_;
@@ -54,11 +54,11 @@ Tooltip.prototype.onAdd = function() {
     // We add an overlay to a map via one of the map's panes.
     // We'll add this overlay to the floatPane pane.
     var panes = this.getPanes();
-  	panes.floatPane.appendChild(this.div_);
-	
-  }
+    panes.floatPane.appendChild(this.div_);
+
+}
 // We here implement draw
-Tooltip.prototype.draw = function() {
+Tooltip.prototype.draw = function () {
 
     // Position the overlay. We use the position of the marker
     // to peg it to the correct position, just northeast of the marker.
@@ -74,22 +74,22 @@ Tooltip.prototype.draw = function() {
     var div = this.div_;
     div.style.left = ne.x + 'px';
     div.style.top = ne.y + 'px';
-    
+
 }
 // We here implement onRemove
-Tooltip.prototype.onRemove = function() {
+Tooltip.prototype.onRemove = function () {
     this.div_.parentNode.removeChild(this.div_);
 }
 
 // Note that the visibility property must be a string enclosed in quotes
-Tooltip.prototype.hide = function() {
+Tooltip.prototype.hide = function () {
     if (this.div_) {
-      this.div_.style.visibility = "hidden";
+        this.div_.style.visibility = "hidden";
     }
 }
 
-Tooltip.prototype.show = function() {
+Tooltip.prototype.show = function () {
     if (this.div_) {
-      this.div_.style.visibility = "visible";
+        this.div_.style.visibility = "visible";
     }
 }
